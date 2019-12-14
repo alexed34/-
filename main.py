0eb62227afe7344c605ed913077aa2b1577e0868
Эдуард <44254815+alexed34@users.noreply.github.com>
2019-12-14 16:21:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import random
 from dotenv import load_dotenv
 
 
-def get_requests(url):
+def get_request(url):
     response = requests.get(url)
     response.raise_for_status()
     response = response.json()
+    raise_for_error_vk(response)
     return response
 
 
@@ -20,7 +21,7 @@ def get_response_comic(num, get_requests):
     return data
 
 
-def write_images(path, data):
+def write_image(path, data):
     with open(os.path.join(path, data['name_image']), 'wb') as f:
         f.write(data['response_image'].content)
 
@@ -35,47 +36,56 @@ def upload_comic_server(name, url):
         return response.json()
 
 
-def save_comic_server(ACCESS_TOKEN, server, photo, hashh):
-    url = 'https://api.vk.com/method/photos.saveWallPhoto?'
-    params = {'group_id': '189760742', 'access_token': ACCESS_TOKEN, 'v': '5.103', 'server': server,
+def save_comic_server(access_token, server, photo, hashh):
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    params = {'group_id': '189760742', 'access_token': access_token, 'v': '5.103', 'server': server,
               'photo': photo, 'hash': hashh}
     response = requests.post(url, params=params)
     response.raise_for_status()
+    raise_for_error_vk(response.json())
     response = response.json().get('response')[0]
     data = {'owner_id': response['owner_id'], 'media_id': response['id']}
     return data
 
 
-def publish_comic_server(ACCESS_TOKEN, data, message):
-    url = 'https://api.vk.com/method/wall.post?'
+def publish_comic_server(access_token, data, message):
+    url = 'https://api.vk.com/method/wall.post'
     attachments = f"photo{str(data['owner_id'])}_{str(data['media_id'])}"
-    params = {'access_token': ACCESS_TOKEN, 'v': '5.103', 'owner_id': '-189760742', 'from_group': '1',
+    params = {'access_token': access_token, 'v': '5.103', 'owner_id': '-189760742', 'from_group': '1',
               'message': message, 'attachments': attachments}
     response = requests.get(url, params=params)
     return response.json()
 
 
+def raise_for_error_vk(response):
+    if 'error' in response:
+        print('error in request in VK')
+        print(response.get('error'))
+
+
 def main():
     load_dotenv()
-    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    access_token = os.getenv('ACCESS_TOKEN')
     path = 'images'
     group_id = '189760742'
     os.makedirs(path, exist_ok=True)
-    num = str(random.randint(1, 2240))
-    data_comic = get_response_comic(num, get_requests)
-    write_images(path, data_comic)
-    server_address_for_downloading_pictures = get_requests \
-        (f'https://api.vk.com/method/photos.getWallUploadServer?group_id={group_id}&access_token={ACCESS_TOKEN}&v=5.103') \
-        .get('response')
-    upload_url = server_address_for_downloading_pictures['upload_url']
-    json_comic_server = upload_comic_server(data_comic['name_image'], upload_url)
-    #print(json_comic_server)
-    server = json_comic_server['server']
-    photo = json_comic_server['photo']
-    hashh = json_comic_server['hash']
-    response_save_comic = save_comic_server(ACCESS_TOKEN, server, photo, hashh)
-    publish_comic_server(ACCESS_TOKEN, response_save_comic, data_comic['comment'])
-    os.remove(f"{path}/{data_comic['name_image']}")
+    current_comic = get_request('https://xkcd.com/info.0.json')['num']
+    num = str(random.randint(1, current_comic))
+    data_comic = get_response_comic(num, get_request)
+    write_image(path, data_comic)
+    try:
+        url = 'https://api.vk.com/method/photos.getWallUploadServer'
+        params = {'group_id': group_id, 'access_token': access_token, 'v': '5.103'}
+        server_address_for_downloading_pictures = get_request(requests.get(url, params=params).url).get('response')
+        upload_url = server_address_for_downloading_pictures['upload_url']
+        data_comic_server = upload_comic_server(data_comic['name_image'], upload_url)
+        server = data_comic_server['server']
+        photo = data_comic_server['photo']
+        str_hash = data_comic_server['hash']
+        response_save_comic = save_comic_server(access_token, server, photo, str_hash, )
+        publish_comic_server(access_token, response_save_comic, data_comic['comment'])
+    finally:
+        os.remove(f"{path}/{data_comic['name_image']}")
 
 
 if __name__ == '__main__':
